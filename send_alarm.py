31b38e8517530efd069a34023b0289fdc1fa022a
root <root@arpwatch-clients.acme-23.test>
2020-04-24 22:40:53 +0000

    modifica

--- send_alarm.py
+++ send_alarm.py
@@ -1,19 +1,42 @@
 import sys
 from socket import *
 import binascii
+import subprocess
+from shlex import split
+
+
+
+
+
+
+
+def run_arpwatch():
+    """run arpwatch"""
+    command = "arpwatch -i eth0 -f /var/lib/arp.dat  -d"
+    process = subprocess.Popen(split(command),stderr=subprocess.PIPE)
+    while True:
+        output, error = process.communicate()
+        if output == '' and process.poll() is not None:
+            break;
+        if output:
+            print output
+    rc = process.poll()
+    return rc
+
+
 
 
 def sendeth (src, dst , type, payload, interface = "eth0"):
-        """send raw ethernet frame on interface"""
-        #48 bit addresses
-        assert(len(src) == len(dst) == 6)
+    """send raw ethernet frame on interface"""
+    #48 bit addresses
+    assert(len(src) == len(dst) == 6)
 
-        #16-bit Ethernet type
-        assert(len(type) == 2)
+    #16-bit Ethernet type
+    assert(len(type) == 2)
 
-        s = socket(AF_PACKET, SOCK_RAW)
-        s.bind((interface,0))
-        return s.send(dst + src + type + payload)
+    s = socket(AF_PACKET, SOCK_RAW)
+    s.bind((interface,0))
+    return s.send(dst + src + type + payload)
 
 def pack(byte_sequence):
     """Convert list of bytes to byte string"""
@@ -21,11 +44,11 @@ def pack(byte_sequence):
 
 
 if __name__=="__main__":
-    print(sys.argv[1])
+    #print(sys.argv[1])
+    """
     source = "ba:9d:d3:ec:ea:07"
     dest = "e6:80:50:76:15:46"
-    #source = source.replace(':', '').decode('hex')
-    #dest = dest.replace(':', '').decode('hex')
+    
     source = binascii.unhexlify(source.replace(':',''))
     dest = binascii.unhexlify(dest.replace(':','')) 
     print(dest, "  ",source)
@@ -40,5 +63,7 @@ if __name__=="__main__":
                                         "\x7A\x05",
                                                   "hello spognardi"))
 
+    """
     
-    
+    run_arpwatch()
+
