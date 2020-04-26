fc01bd12e09a2b0430b588efdf18dfbb8ffbae92
root <root@arpwatch-clients.acme-23.test>
2020-04-26 03:39:40 +0000

    parsing

--- send_alarm.py
+++ send_alarm.py
@@ -5,7 +5,7 @@ import subprocess
 from shlex import split
 
 
-
+host_MAC = binascii.unhexlify("ba:9d:d3:ec:ea:07".replace(':',''))
 
 
 
@@ -13,21 +13,42 @@ from shlex import split
 def run_arpwatch():
     """run arpwatch"""
     command = "arpwatch -i eth0 -f /var/lib/arp.dat  -d"
-    process = subprocess.Popen(split(command),stderr=subprocess.PIPE)
+    process = subprocess.Popen(split(command),stdout=subprocess.PIPE)
+    payload = ""
     while True:
-        output, error = process.communicate()
+        output = process.stdout.readline()
         if output == '' and process.poll() is not None:
             break;
         if output:
-            print output
+            payload += output
+            if "delta" in output: # the report is complitely captured
+                #print payload
+                
+                dst1 = payload.split("ethernet address:",1)[1][:20].strip()
+                dst2 = payload.split("old ethernet address:",1)[1][:20].strip()
+                dst1 = binascii.unhexlify(dst1.replace(':',''))
+                dst2 = binascii.unhexlify(dst2.replace(':',''))
+                sendeth (host_MAC, dst1, payload)
+                sendeth (host_MAC, dst2, payload)
+                #print(dst1)
+                #print( dst2)
+                #print(payload)
+                
+
+                payload = ""
     rc = process.poll()
     return rc
 
 
 
 
-def sendeth (src, dst , type, payload, interface = "eth0"):
+def sendeth (src, dst , payload, interface = "eth0"):
     """send raw ethernet frame on interface"""
+    
+    print(src)
+    print(dst)
+    
+    type = binascii.unhexlify("01FF")
     #48 bit addresses
     assert(len(src) == len(dst) == 6)
 
